e452cb0e0a4c112e8c68b768bd461fcf52eb7272
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-21 09:59:09 +0530

    included SwaggerConfig

---
+++ server/src/main/java/edu/nhs/wsaams/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package edu.nhs.wsaams.config;
+
+/* @author ravin @date 20-05-2020 @time 21:17 */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket getSwaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
